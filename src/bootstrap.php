6218db214864d67c05e74724e674773b4b545d8a
Simon Dann <carbontwelve@users.noreply.github.com>
2017-12-20 11:49:36 +0000

    :sparkles: added basic cli helpers for #2

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -19,5 +19,6 @@ $app->getContainer()->share('config', include __DIR__ . '/../config.php');
 $app->register(new \App\Services\Routes());
 $app->register(new \App\Services\Database());
 $app->register(new \App\Services\Plates());
+$app->register(new \App\Services\Console());
 
 return $app;
