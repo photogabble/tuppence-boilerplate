bd18109e31d21609786dedeb32c7b9ef514a2d36
Simon Dann <carbontwelve@users.noreply.github.com>
2018-04-04 09:46:28 +0100

    Added boilerplate exception handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,6 +5,11 @@ define('APP_ROOT', realpath(__DIR__ . '/../'));
 include APP_ROOT . '/vendor/autoload.php';
 
 $app = new \Photogabble\Tuppence\App();
+try {
+    $app->setExceptionHandler(new \App\Exceptions\Handler($app));
+} catch (\Photogabble\Tuppence\ErrorHandlers\InvalidHandlerException $e) {
+    echo $e->getMessage(); die();
+}
 
 //
 // Config
